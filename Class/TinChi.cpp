4f608e0c5ec84d9df000f64c5ae9594636037fb9
tuanminhhit@gmail.com <6151071072@st.utc2.edu.vn>
2021-11-14 16:52:49 +0700

    first commit

--- Class/TinChi.cpp
+++ Class/TinChi.cpp
@@ -10,6 +10,8 @@ class TinChi : public Diem{
 		int getSoTinChi();
 		friend istream& operator >> (istream& is, TinChi& tc);
 		friend ostream& operator << (ostream& os, TinChi& tc);
+		void doc(ifstream&);
+		void ghi(ofstream&);
 };
 TinChi::TinChi(){
 	SoTinChi = 0;
@@ -18,7 +20,7 @@ TinChi::TinChi(int SoTinChi){
 	this->SoTinChi = SoTinChi;
 }
 TinChi::~TinChi(){
-	
+ 
 }
 void TinChi::setSoTinChi(int SoTinChi){
 	this->SoTinChi = SoTinChi;
@@ -30,16 +32,28 @@ istream& operator >> (istream& is, TinChi& tc){
 	Diem *a = static_cast <Diem *> (&tc);
 	fflush(stdin);
 	is >> *a;
+	int f;
 	cout << "Nhap so tin chi: ";
-	is >> tc.SoTinChi;
+	is >> f;
+	tc.setSoTinChi(f);
 	return is;
 }
 ostream& operator << (ostream& os, TinChi& tc){
 	if(tc.getDiemTong() < 4){
-		os << tc.getHocPhan() << "\t" << tc.getTenHocPhan() << "\t" << tc.getDiemQuaTrinh() << "\t" << tc.getDiemThi()<< "\t" << tc.getDiemTong() << "\t" << tc.SoTinChi << "\t" << "CHUA DAT" << endl;
+		os << left << setw(10) << "|" << setw(17) << tc.getHocPhan() << setw(12) << "|" << setw(39) << tc.getTenHocPhan() << setw(13) << "|" << setw(13) <<  tc.getDiemQuaTrinh() << setw(11) << "|" << setw(12) << tc.getDiemThi() << setw(11) << "|" << setw(13) << tc.getDiemTong() << setw(13) << "|" <<  setw(12) << tc.SoTinChi << setw(9) << "|"  << setw(15) << "CHUA DAT" << "|";
 	}
 	else{
-		os << tc.getHocPhan() << "\t" << tc.getTenHocPhan() << "\t" << tc.getDiemQuaTrinh() << "\t" << tc.getDiemThi()<< "\t" << tc.getDiemTong() << "\t" << tc.SoTinChi << "\t" << "DAT" << endl;
+		os << left << setw(10) << "|" << setw(17) << tc.getHocPhan() << setw(12) << "|" << setw(39) << tc.getTenHocPhan() << setw(13) << "|" << setw(13) <<  tc.getDiemQuaTrinh() << setw(11) << "|" << setw(12) << tc.getDiemThi() << setw(11) << "|" << setw(13) << tc.getDiemTong() << setw(13) << "|" <<  setw(12) << tc.SoTinChi << setw(9) << "|"  << setw(15) << "DAT" <<  "|";
 	}
+	os << "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;	
 	return os;
+}
+void TinChi::doc(ifstream& is){
+	Diem::doc(is);
+	fflush(stdin);
+	int f;
+	is >> f;
+	char ss[5];
+	is.getline(ss, 3);
+	setSoTinChi(f);
 }
